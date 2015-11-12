a3b094eb4f03d0cf5a17f7f0cc044a809540ebc6
henribunting <info@henrib.info>
2015-11-12 16:47:34 +0100

    tanh / cosh

--- E3/MLP_malte.py
+++ E3/MLP_malte.py
@@ -31,9 +31,11 @@ def NNL(hiddenneurons, a_f, a_fderiv, data, run_number):
            break
 
        #Delta_j^v for calculating the weight shift; delta for output is 1 for the identity transfer function
-       d  = 4*np.cosh(np.dot(x,w[0].reshape(1,nhu)))**2/np.cosh(2*np.dot(x,w[0].reshape(1,nhu))+1)**2 * np.dot(np.ones(p).reshape(p,1),w[1].reshape(1,nhu))
+       #d  = 4*np.cosh(np.dot(x,w[0].reshape(1,nhu)))**2/np.cosh(2*np.dot(x,w[0].reshape(1,nhu))+1)**2 * np.dot(np.ones(p).reshape(p,1),w[1].reshape(1,nhu))
+       d  = (1-np.tanh(np.dot(x,w[0].reshape(1,nhu))))**2 * np.dot(np.ones(p).reshape(p,1),w[1].reshape(1,nhu))
        #          bias->hiddenunit                    hiddenunit->output
-       db = 4*np.cosh(b[0])**2/np.cosh(2*b[0]+1)**2 * w[1]
+       #db = 4*np.cosh(b[0])**2/np.cosh(2*b[0]+1)**2 * w[1]
+       db = (1-np.tanh(b[0]))**2
        #d = a_fderiv(np.dot(x, w[0].reshape(1, nhu)))*np.dot(np.ones(p).reshape(p,1),w[1].reshape(1,nhu))
        #weight adjustion
        dw1 = np.mean((o-t)*a,axis=0)
@@ -52,7 +54,7 @@ def NNL(hiddenneurons, a_f, a_fderiv, data, run_number):
     #a
     iterations = range(len(MeanErrors))
     mplt.plot(iterations,MeanErrors)
-    pylab.savefig('./plots/3_1a_'+str(run_number)+'.png', bbox_inches='tight')
+    pylab.savefig('./plots/3_1a_t_'+str(run_number)+'.png', bbox_inches='tight')
     mplt.clf()
 
     
@@ -61,7 +63,7 @@ def NNL(hiddenneurons, a_f, a_fderiv, data, run_number):
     a = np.tanh(np.dot(x,w[0].reshape(1,nhu))-b[0]);
     for i in range(3):
        mplt.plot(x,a[:,i])
-    pylab.savefig('./plots/3_1b_'+str(run_number)+'.png', bbox_inches='tight')
+    pylab.savefig('./plots/3_1b_t_'+str(run_number)+'.png', bbox_inches='tight')
     mplt.clf()
 
 
@@ -72,7 +74,7 @@ def NNL(hiddenneurons, a_f, a_fderiv, data, run_number):
     x = x.reshape(p,1)
     t = t.reshape(p,1)
     mplt.scatter(x,t)
-    pylab.savefig('./plots/3_1c_'+str(run_number)+'.png', bbox_inches='tight')
+    pylab.savefig('./plots/3_1c_t_'+str(run_number)+'.png', bbox_inches='tight')
     mplt.clf()
 
     return o[-1]
