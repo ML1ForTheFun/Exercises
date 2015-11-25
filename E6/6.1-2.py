d0e572460aac35a3db2c08109bb4c182f2158e16
Martin <martin.lundfall@gmail.com>
2015-11-26 00:19:51 +0100

    Contour plots and labels for 6.1!

--- E6/6.1-2.py
+++ E6/6.1-2.py
@@ -21,10 +21,21 @@ data = createDataAsXYL(60)
 #print data
 
 # ------- 6.1 -------
+def find_k_NearestNeighborsIndices(coordinates, k):
+   dist = np.linalg.norm(np.tile(coordinates, (data.shape[0], 1)) - data[:,:-1], axis=1)
+   return dist.argsort()[1:k+1]
+
 def findNearestNeighborIndiciesToScipy(data, pointindex, numberofneighbors):
    t = tree(data[:,:-1])
    return t.query(data[:,:-1][pointindex], numberofneighbors+1)[1][1:]
 
+h = .05  # step size in the mesh
+# create a mesh to plot in
+x_min, x_max = data[:, 0].min() - 0.2, data[:, 0].max() + 0.2
+y_min, y_max = data[:, 1].min() - 0.2, data[:, 1].max() + 0.2
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+allpoints = np.c_[xx.ravel(), yy.ravel()]
 
 for k in [1, 3, 5]:
    classifiedasones = [None] * len(data)
@@ -42,13 +53,19 @@ for k in [1, 3, 5]:
    zeros = data[np.asarray(classifiedasones)==False]
    #print "ones:\n"+str(ones)
    #print "zeros:\n"+str(zeros)
+   #Show the contours of the classifications
+   contours = np.zeros(allpoints.shape[0])
+   for i in range(allpoints.shape[0]):
+      contours[i] = np.sign(np.mean(data[find_k_NearestNeighborsIndices(allpoints[i], k)][:,-1]))
 
-   mplt.scatter(getDataWithLabel(zeros,  1).T[0], getDataWithLabel(zeros,  1).T[1], color='red',  marker='s')        #generated as  1, marked as -1
-   mplt.scatter(getDataWithLabel(ones,   1).T[0], getDataWithLabel(ones,   1).T[1], color='blue', marker='s')        #generated as  1, marked as  1
-   mplt.scatter(getDataWithLabel(zeros, -1).T[0], getDataWithLabel(zeros, -1).T[1], color='blue', marker=r'$\star$') #generated as -1, marked as -1
-   mplt.scatter(getDataWithLabel(ones,  -1).T[0], getDataWithLabel(ones,  -1).T[1], color='red',  marker=r'$\star$') #generated as -1, marked as  1
+   mplt.figure()
+   mplt.scatter(getDataWithLabel(zeros,  1).T[0], getDataWithLabel(zeros,  1).T[1], color='red',  marker='s', label='False negatives')        #generated as  1, marked as -1
+   mplt.scatter(getDataWithLabel(ones,   1).T[0], getDataWithLabel(ones,   1).T[1], color='blue', marker='s', label='True positives')        #generated as  1, marked as  1
+   mplt.scatter(getDataWithLabel(zeros, -1).T[0], getDataWithLabel(zeros, -1).T[1], color='blue', marker=r'$\star$', label='True negatives') #generated as -1, marked as -1
+   mplt.scatter(getDataWithLabel(ones,  -1).T[0], getDataWithLabel(ones,  -1).T[1], color='red',  marker=r'$\star$', label='False positives') #generated as -1, marked as  1
+   mplt.contour(xx, yy, contours.reshape(xx.shape), cmap=mplt.cm.Paired, levels=[0.5]);
+   mplt.legend(loc=2)
    mplt.savefig('6.1b_k='+str(k)+'.png', bbox_inches='tight')
-
 
 
 # ------- 6.2 -------
